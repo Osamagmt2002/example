819c88f2d334257ecee5a8ff97d180415810c468
osamagmt <34580348+osamagmt@users.noreply.github.com>
2022-03-07 02:08:18 +0100

    Update autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'autoplay',
   inVoiceChannel: true,
   run: async (client, message) => {
-if (message.member.voice.channel.id === "950187127021400165")         
+if (message.member.voice.channel.id === "950187127021400165");  
     const queue = client.distube.getQueue(message)
     if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
     const autoplay = queue.toggleAutoplay()
