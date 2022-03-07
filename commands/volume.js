3eb3d46c34687f210884e1706b20a69cea726313
osamagmt <34580348+osamagmt@users.noreply.github.com>
2022-03-07 01:54:30 +0100

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'volume',
-  aliases: ['v', 'set', 'set-volume'],
+  aliases: ['vol', 'set', 'set-volume'],
   inVoiceChannel: true,
   run: async (client, message, args) => {
     const queue = client.distube.getQueue(message)
